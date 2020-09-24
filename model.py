fc3561e0426ae381913da48c811a0c4b24a4da52
jvla97 <52662617+jvla97@users.noreply.github.com>
2020-09-24 12:37:53 +0200

    Up to animation

--- model.py
+++ model.py
@@ -2,80 +2,94 @@ import random
 import operator
 import matplotlib.pyplot
 import agentframework
- 
+import csv
+import sys
 
-def distance_between(agents_row_a, agents_row_b):
-    return (((agents_row_a.x - agents_row_b.x)**2) + 
-    ((agents_row_a.y - agents_row_b.y)**2))**0.5
+#setting parameters
+# num_of_agents = 10
+# num_of_iterations = 100
+# neighbourhood = 20
 
-num_of_agents = 10
-num_of_iterations = 100
+num_of_agents = sys.argv[0]
+num_of_iterations = sys.argv[1]
+neighbourhood = sys.argv[2]
+
+
+environment = []
+f = open('in.txt', newline='') 
+reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+for row in reader: # A list of rows
+    rowlist = [] 
+    for value in row: # A list of value
+        rowlist.append(value)
+        #print(value) # Float
+    #print(rowlist)
+    environment.append(rowlist)
+    
+f.close() # Don't close until you are done with the reader;
+        # the data is read on request.
+size = len(environment)
+print(size)
+"""
+environment = []
+with open('in.txt', newline='') as f:
+    reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+    for row in reader: # A list of rows
+        rowlist = [] 
+        for value in row: # A list of value
+            rowlist.append(value)
+            #print(value) # Float
+        print(rowlist)
+        environment.append(rowlist)
+"""
+
+matplotlib.pyplot.imshow(environment)
+matplotlib.pyplot.show()
+
+
+# def distance_between(agents_row_a, agents_row_b):
+#     return (((agents_row_a.x - agents_row_b.x)**2) + 
+#     ((agents_row_a.y - agents_row_b.y)**2))**0.5
+
+
 agents = []
 
 # Make the agents.
 for i in range(num_of_agents):
-    agents.append(agentframework.Agent())
+    agents.append(agentframework.Agent(environment, size, agents))
 
 # Move the agents.
 for j in range(num_of_iterations):
+    random.shuffle(agents)
     for i in range(num_of_agents):
-
         agents[i].move()
+        agents[i].eat()
+        agents[i].share_with_neighbours(neighbourhood)
 
-matplotlib.pyplot.xlim(0, 99)
-matplotlib.pyplot.ylim(0, 99)
+
+#matplotlib.pyplot.xlim(0, 99)
+#matplotlib.pyplot.ylim(0, 99)
+matplotlib.pyplot.imshow(environment)
 for i in range(num_of_agents):
     matplotlib.pyplot.scatter(agents[i].x,agents[i].y)
 matplotlib.pyplot.show()
 
-for agents_row_a in agents:
-    for agents_row_b in agents:
-        distance = distance_between(agents_row_a, agents_row_b)
-
-
-
-
-
-
-# def distance_between(agents_row_a, agents_row_b):
-#     return (((agents_row_a[0] - agents_row_b[0])**2) + ((agents_row_a[1] - agents_row_b[1])**2))**0.5
-
-
-# num_of_agents = 10
-# num_of_iterations = 100
-# agents = []
-# for i in range(num_of_agents):
-#     agents.append([random.randint(0,100),random.randint(0,100)])
-# # y0, x0
-# print(agents)
-
-# for j in range(num_of_iterations):
-#     for i in range(num_of_agents):
-#         if random.random() < 0.5:
-#             agents[i][0] = (agents[i][0] + 1) % 100
-#         else:
-#             agents[i][0] = (agents[i][0] - 1) % 100
-        
-#         if random.random() < 0.5:
-#             agents[i][1] = (agents[i][1] +1) % 100
-#         else:
-#             agents[i][1] = (agents[i][1] -1) % 100
-
-
-# print(agents)
-# #coord = max(agents, key=operator.itemgetter(1))
-
-# matplotlib.pyplot.ylim(0, 99)
-# matplotlib.pyplot.xlim(0, 99)
-
-# for i in range(num_of_agents):
-#     matplotlib.pyplot.scatter(agents[i][1],agents[i][0])
-
-# #matplotlib.pyplot.scatter(coord[1],coord[0], color = 'red')
-
-# matplotlib.pyplot.show()
-
 # for agents_row_a in agents:
 #     for agents_row_b in agents:
 #         distance = distance_between(agents_row_a, agents_row_b)
+
+#choose arbitrarily any agent to find the updated environment
+#we will choose agent[0]
+#t
+outfile = open('outfile.txt', 'w')
+for line in environment:
+    for value in line:
+        outfile.write(str(value))
+        outfile.write(', ')
+outfile.close()
+
+
+for agent in agents:
+    print(agent)
+
 
